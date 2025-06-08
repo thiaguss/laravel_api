77d46f609e22a9c66f3b82527fe659a04c1853ce
Thiago Santos <tsantos7098@gmail.com>
2025-06-08 12:03:08 -0300

    "Implements user deletion via DELETE method"

--- app/Http/Api/V1/TaskController.php
+++ app/Http/Api/V1/TaskController.php
@@ -50,6 +50,8 @@ class TaskController extends Controller
      */
     public function destroy(Task $task)
     {
-        //
+        $task->delete();
+
+        return response()->noContent();
     }
 }
